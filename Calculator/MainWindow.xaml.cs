7ac7a57032c57567d6f20e7ce5c846634205d5fe
Eduardo Rosas <lalorosas@outlook.com>
2020-09-14 21:59:11 -0500

    Displaying a Message Box

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -142,6 +142,11 @@ namespace Calculator
 
         public static double Divide(double dividend, double divisor)
         {
+            if(divisor == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported.", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
             return dividend / divisor;
         }
     }
